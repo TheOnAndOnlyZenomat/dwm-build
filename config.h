e320ac9b48d9d2bf31a1371e69ee2e2e62fe99d8
TheOnAndOnlyZenomat <eichelstein@online.de>
2020-07-20 17:19:06 +0200

    for backup

--- config.h
+++ config.h
@@ -8,20 +8,30 @@ static const int swallowfloating    = 0; 	/* 1 means swallow floating windows by
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 30;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 50;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 50;       /* vert outer gap between windows and screen edge */
+//static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+//static const unsigned int gappiv    = 30;       /* vert inner gap between windows */
+//static const unsigned int gappoh    = 50;       /* horiz outer gap between windows and screen edge */
+//static const unsigned int gappov    = 50;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 10;
+static const unsigned int gappiv    = 10;
+static const unsigned int gappoh    = 10;
+static const unsigned int gappov    = 10; 
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Ubuntu Nerd Font:size=11:antialias=true", "JoyPixels:pixelsize=11:antialias=true" };
 static const char dmenufont[]       = "Hack Nerd Font:size=10";
-static const char col_gray1[]       = "#0B0C08";
+/*static const char col_gray1[]       = "#0B0C08";
 static const char col_gray2[]       = "#1F1910";
 static const char col_gray3[]       = "#082E2B";
 static const char col_gray4[]       = "#541405";
 static const char col_cyan[]        = "#C34609";
+*/
+static const char col_gray1[]	    = "#1D3052";
+static const char col_gray2[]	    = "#0C172C";
+static const char col_gray3[]	    = "#364A6C";
+static const char col_gray4[]	    = "#0D131F";
+static const char col_cyan[]	    = "#556C90";
 static const char col_white[]	    = "#ffffff";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
