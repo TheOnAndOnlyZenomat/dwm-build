f5a1730d15c439e194790b78646b5d175f2c4929
TheOnAndOnlyZenomat <eichelstein@online.de>
2020-08-01 12:05:45 +0200

    changed colorscheme

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 2;   /* 0: sloppy systray follows selected monitor >0: pin systray to monitor X */
 static const int swallowfloating    = 0; 	/* 1 means swallow floating windows by default */
@@ -12,10 +12,10 @@ static const int showsystray        = 1;     /* 0 means no systray */
 //static const unsigned int gappiv    = 30;       /* vert inner gap between windows */
 //static const unsigned int gappoh    = 50;       /* horiz outer gap between windows and screen edge */
 //static const unsigned int gappov    = 50;       /* vert outer gap between windows and screen edge */
-static const unsigned int gappih    = 10;
-static const unsigned int gappiv    = 10;
-static const unsigned int gappoh    = 10;
-static const unsigned int gappov    = 10; 
+static const unsigned int gappih    = 5;
+static const unsigned int gappiv    = 5;
+static const unsigned int gappoh    = 5;
+static const unsigned int gappov    = 5;
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -26,22 +26,30 @@ static const char col_gray2[]       = "#1F1910";
 static const char col_gray3[]       = "#082E2B";
 static const char col_gray4[]       = "#541405";
 static const char col_cyan[]        = "#C34609";
-*/
+
 static const char col_gray1[]	    = "#1D3052";
 static const char col_gray2[]	    = "#0C172C";
 static const char col_gray3[]	    = "#364A6C";
 static const char col_gray4[]	    = "#0D131F";
 static const char col_cyan[]	    = "#556C90";
+*/
+
+static const char col_gray1[]	    = "#1c1e26";
+static const char col_gray2[]	    = "#0C172C";
+static const char col_gray3[]	    = "#e93c58";
+static const char col_gray4[]	    = "#0D131F";
+static const char col_cyan[]	    = "#414658";
+
 static const char col_white[]	    = "#ffffff";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeStatus]  = { col_white, col_gray4,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray3  },
+	[SchemeStatus]  = { col_gray3, col_gray1,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
 	[SchemeTagsSel]  = { col_gray4, col_cyan,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
-    	[SchemeTagsNorm]  = { col_gray3, col_gray1,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
-    	[SchemeInfoSel]  = { col_gray4, col_cyan,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
-    	[SchemeInfoNorm]  = { col_gray3, col_cyan,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
+    [SchemeTagsNorm]  = { col_gray3, col_gray1,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+    [SchemeInfoSel]  = { col_gray4, col_cyan,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+    [SchemeInfoNorm]  = { col_gray3, col_cyan,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 static char* const autostart[][2] = { /* please replace 2 with maximum number of arguments from autostart array */
