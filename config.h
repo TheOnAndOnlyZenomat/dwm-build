dcd2c828e8b08c10435cb67a038039e46d2c7de9
TheOnAndOnlyZenomat <eichelstein@online.de>
2020-12-15 13:12:34 +0100

    update

--- config.h
+++ config.h
@@ -1,27 +1,29 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int snap      = 10;       /* snap pixel */
 static const unsigned int systraypinning = 2;   /* 0: sloppy systray follows selected monitor >0: pin systray to monitor X */
 static const int swallowfloating    = 0; 	/* 1 means swallow floating windows by default */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
-//static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-//static const unsigned int gappiv    = 30;       /* vert inner gap between windows */
-//static const unsigned int gappoh    = 50;       /* horiz outer gap between windows and screen edge */
-//static const unsigned int gappov    = 50;       /* vert outer gap between windows and screen edge */
-static const unsigned int gappih    = 5;
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 30;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 50;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 50;       /* vert outer gap between windows and screen edge */
+/*static const unsigned int gappih    = 5;
 static const unsigned int gappiv    = 5;
 static const unsigned int gappoh    = 5;
 static const unsigned int gappov    = 5;
+*/
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Ubuntu Nerd Font:size=11:antialias=true", "JoyPixels:pixelsize=11:antialias=true" };
+static const char *fonts[]          = { "Tamzen:size=12:antialias=true", "JoyPixels:pixelsize=11:antialias=true" };
 static const char dmenufont[]       = "Hack Nerd Font:size=10";
-/*static const char col_gray1[]       = "#0B0C08";
+/*
+static const char col_gray1[]       = "#0B0C08";
 static const char col_gray2[]       = "#1F1910";
 static const char col_gray3[]       = "#082E2B";
 static const char col_gray4[]       = "#541405";
@@ -34,11 +36,22 @@ static const char col_gray4[]	    = "#0D131F";
 static const char col_cyan[]	    = "#556C90";
 */
 
+/* Horizon Dark */
+
 static const char col_gray1[]	    = "#1c1e26";
 static const char col_gray2[]	    = "#0C172C";
 static const char col_gray3[]	    = "#e93c58";
 static const char col_gray4[]	    = "#0D131F";
 static const char col_cyan[]	    = "#414658";
+
+
+/*
+static const char col_gray1[]	    = "#635F57";
+static const char col_gray2[]	    = "#888771";
+static const char col_gray3[]	    = "#B4B1AF";
+static const char col_gray4[]	    = "#494939";
+static const char col_cyan[]	    = "#31312F";
+*/
 
 static const char col_white[]	    = "#ffffff";
 static const char *colors[][3]      = {
@@ -69,10 +82,11 @@ static const Rule rules[] = {
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "st",      NULL,     NULL,           0,         0,          1,          -1,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
+	{ "albert",  NULL,     "albert",           0,         1,          0,          -1,        -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
